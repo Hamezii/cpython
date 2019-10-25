f3baf68a1e198a75ca51e2d7bc3a73627df580e0
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-10-25 12:12:13 +0100

    Improvements to native pointers: allow creating from NULL/False/0

--- Include/pointerobject.h
+++ Include/pointerobject.h
@@ -17,14 +17,24 @@ static inline _Bool PyNativePointer_IsNull(PyObject* x) {
     /* Also allow NULL pointers */
     if (x == Py_None || x == Py_False)
         return 1;
-    if (PyLong_Check(x) && _PyLong_Sign(x) == 0)
-        return 1;
+    if (PyLong_Check(x))
+        return _PyLong_Sign(x) == 0;
+    /* TODO: do we need this fallback? */
+#if 0
     /* Otherwise convert to long and check if the value is zero */
-    return PyLong_AsLong(x) == 0;
+    PyObject * aslong = _PyLong_FromNbIndexOrNbInt(x);
+    /* Ignore conversion errors */
+    if (PyErr_Occurred())
+        PyErr_Clear();
+    if (aslong && _PyLong_Sign(aslong) == 0)
+        return 1;
+#endif
+     return 0;
 }
 /* Function to return a bool from a C long */
 PyAPI_FUNC(PyObject *) PyNativePointer_FromVoidPointer(void *);
 PyAPI_FUNC(void *) PyNativePointer_AsVoidPointer(PyObject *);
+PyAPI_FUNC(uintptr_t) PyNativePointer_AsUIntPtr(PyObject *);
 
 #ifdef __cplusplus
 }
