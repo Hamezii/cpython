11251619024a3b32003c1d298bc7fd57097353b5
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-10-25 12:11:22 +0100

    Fix c_void_p

--- Modules/_ctypes/_ctypes.c
+++ Modules/_ctypes/_ctypes.c
@@ -1835,7 +1835,7 @@ c_void_p_from_param(PyObject *type, PyObject *value)
     }
     /* Should probably allow buffer interface as well */
 /* int, long */
-    if (PyLong_Check(value)) {
+    if (PyLong_Check(value) || PyNativePointer_Check(value)) {
         PyCArgObject *parg;
         struct fielddesc *fd = _ctypes_get_fielddesc("P");
 
@@ -3731,6 +3731,7 @@ PyCFuncPtr_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
         if (ob == NULL)
             return NULL;
         *(void **)ob->b_ptr = ptr;
+        assert(!PyErr_Occurred());
         return (PyObject *)ob;
     }
 
@@ -3757,6 +3758,7 @@ PyCFuncPtr_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
         return NULL;
     }
 */
+    assert(!PyErr_Occurred());
 
     dict = PyType_stgdict((PyObject *)type);
     /* XXXX Fails if we do: 'PyCFuncPtr(lambda x: x)' */
@@ -3791,6 +3793,7 @@ PyCFuncPtr_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
         Py_DECREF((PyObject *)self);
         return NULL;
     }
+    assert(!PyErr_Occurred());
     return (PyObject *)self;
 }
 
