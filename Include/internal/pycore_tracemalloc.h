9cc2e316e7b43e44b1018df0646d0a3d0d7adba8
Alfredo Mazzinghi <alfredo.mazzinghi@gmail.com>
2024-02-21 16:44:08 +0000

    Fix mismerge, do not pack struct tracemalloc_frame.
    This was lost due to a file rename.

--- Include/internal/pycore_tracemalloc.h
+++ Include/internal/pycore_tracemalloc.h
@@ -41,7 +41,7 @@ struct _PyTraceMalloc_Config {
 #endif
 
 struct
-#ifdef __GNUC__
+#if defined(__GNUC__) && !defined(__CHERI_PURE_CAPABILITY__)
 __attribute__((packed))
 #endif
 tracemalloc_frame {
